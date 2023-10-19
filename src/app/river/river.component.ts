63f20158349c7567daf21281697be1ce19096319
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-10-19 08:43:50 +0200

    Opzet

---
+++ src/app/river/river.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-river',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      river works!
+    </p>
+  `,
+  styleUrls: ['./river.component.css']
+})
+export class RiverComponent {
+
+}
